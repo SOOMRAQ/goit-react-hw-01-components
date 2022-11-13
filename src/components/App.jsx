bfdd26e3be36c1c6edbf7c5598411f6ef7c1e813
Nikita Taranenko <soomraq.x@gmail.com>
2022-11-13 23:53:35 +0200

    add css to task 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,7 +21,7 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      {/* <Statistics stats={data} /> */}
       <FriendsList friends={friends} />;
       <TransactionHistory items={transactions} />;
     </div>
