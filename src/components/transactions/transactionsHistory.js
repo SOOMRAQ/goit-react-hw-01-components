8d944bdbb85068fd1752fbccbbb2a3ed1cdddc4c
Nikita Taranenko <soomraq.x@gmail.com>
2022-11-14 00:50:58 +0200

    add css to task 4

---
+++ src/components/transactions/transactionsHistory.js
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types';
+import transactions from '../transactions/transactions';
+import css from 'components/transactions/transactions.module.css';
+
+const TransactionHistory = items => {
+  return (
+    <table className={css.transactions}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {transactions.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      type: PropTypes.string,
+      amount: PropTypes.string,
+      currency: PropTypes.string,
+    })
+  ),
+};
+
+export default TransactionHistory;
